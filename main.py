b67615bd5253092761baf8f22dc2ec4b7809932a
Alexis Trupin <link9745@gmail.com>
2020-09-12 16:49:13 +0200

    adding train/test split

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from requests import get
 from PIL import Image
 from io import BytesIO
 from numpy import concatenate
+from math import floor
 from sklearn.neural_network import MLPClassifier
 from joblib import dump
 
@@ -23,6 +24,18 @@ with open('players.csv') as csvfile:
         in_rows.append(array)
         out_rows.append(gender)
 
+split = floor(len(in_rows) * 0.80)
+
+in_train_rows = in_rows[:split]
+out_train_rows = out_rows[:split]
+
 clf = MLPClassifier(hidden_layer_sizes=(20, 20), solver='adam', alpha=0.0002, verbose=True)
-clf.fit(in_rows, out_rows)
+clf.fit(in_train_rows, out_train_rows)
+
+in_test_rows = in_rows[split:]
+out_test_rows = out_rows[split:]
+
+score = clf.score(in_test_rows, out_test_rows)
+print(f'Test score is {score}')
+
 dump(clf, 'model.jlb')
